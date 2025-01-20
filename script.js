033f0c420a36d8a3c612e7d891d1704db19767e2
Aymen <aymenghaloua@gmail.com>
2025-01-20 14:23:33 +0100

    Complete

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+let playerState = 'fall';
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change', (e) => {
+  playerState = e.target.value;
+  frameX = 0;
+});
+
+const animationStates = {
+  idle: { frames: 7 },
+  jump: { frames: 7 },
+  fall: { frames: 7 },
+  run: { frames: 9 },
+  dizzy: { frames: 11 },
+  sit: { frames: 5 },
+  roll: { frames: 7 },
+  bite: { frames: 7 },
+  ko: { frames: 12 },
+  getHit: { frames: 4 },
+};
+
+const spriteWidth = 6876 / 12;
+const spriteHeight = 5230 / 10;
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -6,25 +29,19 @@ const CANVAS_HEIGHT = (canvas.height = 600);
 
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
-const spriteWidth = 6876 / 12;
-const spriteHeight = 5230 / 10;
 
-let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
-const staggerFrames = 5;
-const spriteAnimations = [];
-const animationStates = [
-  { name: 'idle', frames: 7 },
-  { name: 'jump', frames: 7 },
-];
-animationStates.forEach((state, index) => {});
+const staggerFrames = 3;
+let frameX = 0;
+let frameY = Object.keys(animationStates).indexOf(playerState) * spriteHeight;
 
 (function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  let position = Math.floor(gameFrame / staggerFrames) % 6;
-  frameX = spriteWidth * position;
-  ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  ctx.drawImage(playerImage, frameX * spriteWidth, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  if (gameFrame % staggerFrames === 0) {
+    if (frameX < animationStates[playerState].frames - 1) frameX++;
+    else frameX = 0;
+  }
   gameFrame++;
   requestAnimationFrame(animate);
 })();
